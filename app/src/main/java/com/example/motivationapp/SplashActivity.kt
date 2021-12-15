64f72793a2f34debe5c2fbde065f3dc63877c4a8
Matheus Victor <matheusvictor.salles@gmail.com>
2021-12-14 21:23:02 -0300

    Add listener to button

--- app/src/main/java/com/example/motivationapp/SplashActivity.kt
+++ app/src/main/java/com/example/motivationapp/SplashActivity.kt
@@ -2,8 +2,10 @@ package com.example.motivationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -11,5 +13,22 @@ class SplashActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+
+        val buttonSend = findViewById<Button>(R.id.buttonSend)
+
+        // Listening this context
+        buttonSend.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        val view_id = view.id
+        if (view_id == R.id.buttonSend) {
+            handleSend()
+        }
+    }
+
+    private fun handleSend() {
+        TODO()
     }
 }
