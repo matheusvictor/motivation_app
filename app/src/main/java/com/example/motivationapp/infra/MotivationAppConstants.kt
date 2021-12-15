1ca248a6070a532359749bb4c2e08e43a01c9bb2
Matheus Victor <matheusvictor.salles@gmail.com>
2021-12-15 11:35:51 -0300

    Add class for constants values

---
+++ app/src/main/java/com/example/motivationapp/infra/MotivationAppConstants.kt
@@ -0,0 +1,12 @@
+package com.example.motivationapp.infra
+
+class MotivationAppConstants private constructor() {
+
+    /**
+     * The private constructor does not allow this class can be instantiate
+     */
+
+    object KEY {
+        val USER_NAME = "username"
+    }
+}
