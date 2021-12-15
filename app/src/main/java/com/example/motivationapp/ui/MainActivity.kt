842f93167d43c6d844ee79cdb4d998a579d6331a
Matheus Victor <matheusvictor.salles@gmail.com>
2021-12-15 14:20:32 -0300

    Get username into MainActivity

--- app/src/main/java/com/example/motivationapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/motivationapp/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.motivationapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import com.example.motivationapp.R
+import com.example.motivationapp.infra.MotivationAppConstants
+import com.example.motivationapp.infra.SecurityPreferences
 
 class MainActivity : AppCompatActivity() {
+
+    // Initialize mSecurityPreferences
+    private lateinit var mSecurityPreferences: SecurityPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Instance mSecurityPreferences
+        mSecurityPreferences = SecurityPreferences(this)
+
+        val textUsername = findViewById<TextView>(R.id.textUsername)
+
+        val username = mSecurityPreferences.getStoredString(MotivationAppConstants.KEY.USER_NAME)
+
+        textUsername.text = "Hello, ${username}"
     }
 }
