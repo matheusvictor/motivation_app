3826047bbc8093c986ec2eb31ef5e19d34f8442b
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-10 13:19:39 -0300

    feat: Turn class to Room Entity

--- app/src/main/java/com/example/motivationapp/model/Phrase.kt
+++ app/src/main/java/com/example/motivationapp/model/Phrase.kt
@@ -1,7 +1,17 @@
 package com.example.motivationapp.model
 
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity
+@Parcelize
 data class Phrase(
-    val description: String,
-    val author: String? = null,
-    val category: Int,
-)
+    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    @ColumnInfo(name = "url_image") val urlImage: String? = null,
+    @ColumnInfo(name = "phrase_text") val text: String,
+    @ColumnInfo(name = "phrase_author") val author: String? = null,
+    @ColumnInfo(name = "phrase_category") val category: Int,
+) : Parcelable
