7b233f6007d454c86f1a475be1d1dd9ea075853d
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-10 09:16:12 -0300

    feat: Add local database configurations

---
+++ app/src/main/java/com/example/motivationapp/repository/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.motivationapp.repository
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.motivationapp.model.Phrase
+
+@Database(entities = [Phrase::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun phrasesDao(): PhrasesDAO
+
+    companion object {
+        fun getInstance(context: Context): AppDatabase {
+            return Room.databaseBuilder(
+                context,
+                AppDatabase::class.java,
+                "phrases.db"
+            ).allowMainThreadQueries()
+                .build()
+        }
+    }
+
+}
