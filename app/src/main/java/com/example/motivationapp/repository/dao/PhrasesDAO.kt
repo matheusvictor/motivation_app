e58f247549f17965b538fe33f70e45afc63de662
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-11 10:06:14 -0300

    feat: Add new queries to list phrases
    - Find all Phrases
    - Find a Phrase by ID
    - Find a Phrase by Category ID

--- app/src/main/java/com/example/motivationapp/repository/dao/PhrasesDAO.kt
+++ app/src/main/java/com/example/motivationapp/repository/dao/PhrasesDAO.kt
@@ -3,6 +3,7 @@ package com.example.motivationapp.repository.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.motivationapp.model.Phrase
 
 @Dao
@@ -10,5 +11,14 @@ interface PhrasesDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun save(vararg phrase: Phrase)
+
+    @Query("SELECT * FROM Phrase")
+    fun findAll(): List<Phrase>
+
+    @Query("SELECT * FROM Phrase WHERE id = :id")
+    fun findById(id: Long): Phrase?
+
+    @Query("SELECT * FROM Phrase WHERE phrase_category = :categoryId")
+    fun findByCategoryId(categoryId: Int): Phrase?
 
 }
