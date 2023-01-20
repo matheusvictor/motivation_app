d5ae984e28ed813fea2b642568a7b2217e3ce2f8
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-20 10:21:18 -0300

    refactor: Implements viewModel concept

--- app/src/main/java/com/example/motivationapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/motivationapp/ui/activity/MainActivity.kt
@@ -2,43 +2,35 @@ package com.example.motivationapp.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.motivationapp.R
 import com.example.motivationapp.databinding.ActivityMainBinding
 import com.example.motivationapp.extensions.tryLoadImage
-import com.example.motivationapp.infra.MotivationAppConstants
+import com.example.motivationapp.infra.MotivationAppConstants.KEY.USER_NAME
 import com.example.motivationapp.infra.SecurityPreferences
-import com.example.motivationapp.model.Phrase
-import com.example.motivationapp.repository.AppDatabase
-import kotlin.random.Random
 
-class MainActivity : AppCompatActivity(R.layout.activity_main), View.OnClickListener {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var viewModel: MainViewModel
+    private val bindingMainActivity by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     // Initialize mSecurityPreferences
     private lateinit var mSecurityPreferences: SecurityPreferences
-    private var mPhraseFiltered: Int = MotivationAppConstants.PHRASES_FILTER.ALL
-
-    private val bindingMainActivity by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
-
-    private val phrasesDAO by lazy {
-        AppDatabase.getInstance(this).phrasesDao()
-    }
-
-    private var phraseFounded: Phrase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(bindingMainActivity.root)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
         // Instance mSecurityPreferences
         mSecurityPreferences = SecurityPreferences(this)
+        bindingMainActivity.username.text = mSecurityPreferences.getStoredString(USER_NAME)
 
-        bindingMainActivity.allCategoriesFilter.setColorFilter(R.color.colorAccent)
         bindingMainActivity.allCategoriesFilter.setColorFilter(resources.getColor(R.color.colorAccent))
 
         bindingMainActivity.allCategoriesFilter.setOnClickListener(this)
@@ -49,7 +41,8 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), View.OnClickList
 
     override fun onResume() {
         super.onResume()
-        generateNewPhrase()
+        showNewPhrase()
+        Log.d("MainActivity", viewModel.phraseFounded.toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -73,15 +66,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), View.OnClickList
     }
 
     override fun onClick(view: View) {
-        val listWithFiltersID = listOf(
+        val categoryFilters = listOf(
             R.id.all_categories_filter,
             R.id.good_vibes_category_filter,
             R.id.bad_vibes_category_filter
         )
 
         if (view.id == R.id.button_see_new_phrase) {
-            generateNewPhrase()
-        } else if (view.id in listWithFiltersID) {
+            showNewPhrase()
+        } else if (view.id in categoryFilters) {
             handleFilter(view.id)
         }
     }
@@ -95,43 +88,35 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), View.OnClickList
         when (filter_value) {
             R.id.all_categories_filter -> {
                 bindingMainActivity.allCategoriesFilter.setColorFilter(resources.getColor(R.color.colorAccent))
-                mPhraseFiltered = MotivationAppConstants.PHRASES_FILTER.ALL
+                viewModel.filterByRandomCategory()
+                Log.d("MainActivity", viewModel.categoryFilter.toString())
             }
             R.id.bad_vibes_category_filter -> {
                 bindingMainActivity.badVibesCategoryFilter.setColorFilter(resources.getColor(R.color.colorAccent))
-                mPhraseFiltered = MotivationAppConstants.PHRASES_FILTER.BAD_VIBES
+                viewModel.filterByBadVibesCategory()
+                Log.d("MainActivity", viewModel.categoryFilter.toString())
             }
             R.id.good_vibes_category_filter -> {
                 bindingMainActivity.goodVibesCategoryFilter.setColorFilter(resources.getColor(R.color.colorAccent))
-                mPhraseFiltered = MotivationAppConstants.PHRASES_FILTER.GOOD_VIBES
+                viewModel.filterByGoodVibesCategory()
+                Log.d("MainActivity", viewModel.categoryFilter.toString())
             }
         }
     }
 
-    private fun generateNewPhrase() {
-
-        phraseFounded = when (mPhraseFiltered) {
-            1 -> {
-                val randCategory = Random.nextInt(
-                    from = MotivationAppConstants.PHRASES_FILTER.GOOD_VIBES,
-                    until = MotivationAppConstants.PHRASES_FILTER.BAD_VIBES + 1
-                )
-                phrasesDAO.findByCategoryId(randCategory)
-            }
-            else -> {
-                phrasesDAO.findByCategoryId(mPhraseFiltered)
-            }
-        }
-
-        fillPhraseFields(phraseFounded)
+    private fun showNewPhrase() {
+        viewModel.getNextPhrase(viewModel.categoryFilter)
+        Log.d("MainActivity", viewModel.categoryFilter.toString())
+        updatePhraseDetails()
     }
 
-    private fun fillPhraseFields(phrase: Phrase?) {
-        bindingMainActivity.phraseMessage.text = phrase?.text ?: "There's nothing here"
-        bindingMainActivity.phraseAuthor.text = phrase?.author ?: "Unknown"
-        bindingMainActivity.phraseImage.tryLoadImage(phrase?.urlImage)
+    private fun updatePhraseDetails() {
+        bindingMainActivity.phraseMessage.text =
+            viewModel.phraseFounded?.text ?: "There isn't nothing to show here"
+        bindingMainActivity.phraseAuthor.text = viewModel.phraseFounded?.author ?: "Unknown"
+        bindingMainActivity.phraseImage.tryLoadImage(viewModel.phraseFounded?.urlImage)
 
-        if (phrase?.urlImage.isNullOrBlank()) {
+        if (viewModel.phraseFounded?.urlImage.isNullOrBlank()) {
             bindingMainActivity.phraseImage.visibility = View.GONE
         } else {
             bindingMainActivity.phraseImage.visibility = View.VISIBLE
