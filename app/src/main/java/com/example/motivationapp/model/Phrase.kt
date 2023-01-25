b091e72adb9656a45c568829e31c7afeeb842271
Matheus Victor <matheusvictor.salles@gmail.com>
2023-01-25 09:20:23 -0300

    feat: Add populated initial database

--- app/src/main/java/com/example/motivationapp/model/Phrase.kt
+++ app/src/main/java/com/example/motivationapp/model/Phrase.kt
@@ -6,12 +6,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
-@Entity
+@Entity(tableName = "phrase")
 @Parcelize
 data class Phrase(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Long = 0L,
     @ColumnInfo(name = "url_image") val urlImage: String? = null,
-    @ColumnInfo(name = "text") val text: String,
+    @ColumnInfo(name = "phrase_message") val text: String,
     @ColumnInfo(name = "author") val author: String? = null,
     @ColumnInfo(name = "category") val category: Int,
 ) : Parcelable
--- app/src/main/java/com/example/motivationapp/repository/AppDatabase.kt
+++ app/src/main/java/com/example/motivationapp/repository/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.example.motivationapp.model.Phrase
 import com.example.motivationapp.repository.dao.PhrasesDAO
 
-@Database(entities = [Phrase::class], version = 1)
+@Database(entities = [Phrase::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun phrasesDao(): PhrasesDAO
 
@@ -18,6 +18,7 @@ abstract class AppDatabase : RoomDatabase() {
                 AppDatabase::class.java,
                 "phrases.db"
             ).allowMainThreadQueries()
+                .createFromAsset("database/phrases.db")
                 .build()
         }
     }
