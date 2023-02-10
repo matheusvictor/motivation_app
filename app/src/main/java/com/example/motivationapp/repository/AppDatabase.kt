91a3bcaf2e858bcd8bdfe9b4f7e7b0685e453ebf
Matheus Victor <matheusvictor.salles@gmail.com>
2023-02-10 11:12:34 -0300

    refactor: Singleton pattern for db instance

--- app/src/main/java/com/example/motivationapp/repository/AppDatabase.kt
+++ app/src/main/java/com/example/motivationapp/repository/AppDatabase.kt
@@ -9,17 +9,25 @@ import com.example.motivationapp.repository.dao.PhrasesDAO
 
 @Database(entities = [Phrase::class], version = 1, exportSchema = true)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun phrasesDao(): PhrasesDAO
 
     companion object {
+        private lateinit var INSTANCE: AppDatabase
+
         fun getInstance(context: Context): AppDatabase {
-            return Room.databaseBuilder(
-                context,
-                AppDatabase::class.java,
-                "phrases.db"
-            ).allowMainThreadQueries()
-                .createFromAsset("database/phrases.db")
-                .build()
+            if (!::INSTANCE.isInitialized) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        "phrases.db"
+                    ).allowMainThreadQueries()
+                        .createFromAsset("database/phrases.db")
+                        .build()
+                }
+            }
+            return INSTANCE
         }
     }
 
