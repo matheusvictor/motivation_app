0e20d88a5038be8fc0e9e6584847e794161a1d80
Matheus Victor <matheusvictor.salles@gmail.com>
2023-02-10 11:14:41 -0300

    refactor: Modify attributes

--- app/src/main/java/com/example/motivationapp/model/Phrase.kt
+++ app/src/main/java/com/example/motivationapp/model/Phrase.kt
@@ -9,9 +9,9 @@ import kotlinx.android.parcel.Parcelize
 @Entity(tableName = "phrase")
 @Parcelize
 data class Phrase(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Long = 0L,
-    @ColumnInfo(name = "url_image") val urlImage: String? = null,
-    @ColumnInfo(name = "phrase_message") val text: String,
-    @ColumnInfo(name = "author") val author: String? = null,
-    @ColumnInfo(name = "category") val category: Int,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") var id: Long = 0L,
+    @ColumnInfo(name = "url_image") var urlImage: String? = null,
+    @ColumnInfo(name = "phrase_message") var text: String,
+    @ColumnInfo(name = "author") var author: String? = null,
+    @ColumnInfo(name = "category") var category: Int,
 ) : Parcelable
