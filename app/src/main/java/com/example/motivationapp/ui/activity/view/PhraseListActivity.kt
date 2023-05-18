1151863cbbd83cbbccff623285af870ef18f3c80
Matheus Victor <matheusvictor.salles@gmail.com>
2023-05-18 09:21:11 -0300

    refactor: Modify phrase list and item layout

--- app/src/main/java/com/example/motivationapp/ui/activity/view/PhraseListActivity.kt
+++ app/src/main/java/com/example/motivationapp/ui/activity/view/PhraseListActivity.kt
@@ -1,11 +1,15 @@
 package com.example.motivationapp.ui.activity.view
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.motivationapp.R
 import com.example.motivationapp.constants.PHRASE_ID
 import com.example.motivationapp.databinding.ActivityPhraseListBinding
@@ -13,6 +17,7 @@ import com.example.motivationapp.databinding.RemoveItemDialogBinding
 import com.example.motivationapp.model.Phrase
 import com.example.motivationapp.repository.AppDatabase
 import com.example.motivationapp.ui.recyclerview.adapter.PhraseListAdapter
+import com.example.motivationapp.ui.recyclerview.adapter.ViewPagerAdapter
 
 class PhraseListActivity : AppCompatActivity(R.layout.activity_phrase_details),
     PhraseListAdapter.LongClickedItem {
@@ -35,11 +40,18 @@ class PhraseListActivity : AppCompatActivity(R.layout.activity_phrase_details),
     override fun onResume() {
         super.onResume()
         adapter.update(phrasesDAO.findAll())
+        setDotsViewPager()
     }
 
     private fun setRecyclerView() {
         val recyclerView = bindingPhraseListActivity.rvPhraseList
         recyclerView.adapter = adapter
+
+        // Configurando o LinearLayoutManager com orientação horizontal
+        val layoutManager =
+            NonScrollableLinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+
         adapter.whenClickOnItem = {
             val intent = Intent(
                 this, PhraseDetailsActivity::class.java
@@ -47,6 +59,46 @@ class PhraseListActivity : AppCompatActivity(R.layout.activity_phrase_details),
                 putExtra(PHRASE_ID, it.id)
             }
             startActivity(intent)
+        }
+    }
+
+    private inner class NonScrollableLinearLayoutManager(
+        context: Context,
+        orientation: Int,
+        reverseLayout: Boolean
+    ) :
+        LinearLayoutManager(context, orientation, reverseLayout) {
+        override fun canScrollHorizontally(): Boolean {
+            return false
+        }
+    }
+
+    private fun setDotsViewPager() {
+        val dotsRecyclerView: RecyclerView = bindingPhraseListActivity.rvViewPagerPhrase
+        val prev = bindingPhraseListActivity.ivPrevPhrase
+        val next = bindingPhraseListActivity.ivNextPhrase
+
+        if (adapter.itemCount > 0) {
+            dotsRecyclerView.adapter =
+                ViewPagerAdapter(context = this, mDotsQuant = adapter.itemCount)
+
+            // Configurando o LinearLayoutManager com orientação horizontal
+            val layoutManager =
+                NonScrollableLinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+            dotsRecyclerView.layoutManager = layoutManager
+
+            prev.setOnClickListener {
+                Toast.makeText(this, "Left", Toast.LENGTH_SHORT).show()
+            }
+
+            next.setOnClickListener {
+                Toast.makeText(this, "Next", Toast.LENGTH_SHORT).show()
+            }
+
+        } else {
+            dotsRecyclerView.visibility = View.GONE
+            prev.visibility = View.GONE
+            next.visibility = View.GONE
         }
     }
 
